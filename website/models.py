9c599488893731590506ad80723dbf72f4366721
Assassin <90865095+assassin65170@users.noreply.github.com>
2022-02-03 02:48:47 +1000

    Update models.py

--- website/models.py
+++ website/models.py
@@ -18,7 +18,7 @@ class Users(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50),nullable=False, unique=True)
-    password_hash = db.Column(db.String(length=50),nullable=False)
+    password_hash = db.Column(db.String(length=200),nullable=False)
 
 
     @property
