943cd26b33a066dc1c011331f8f878e8eb84f268
Assassin <90865095+assassin65170@users.noreply.github.com>
2022-02-02 21:25:59 +1000

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os 
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
